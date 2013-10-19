64356d35e4ca9d18d6858d4765f0db9893460981
Yinan Yang <subzerodigital@gmail.com>
2013-10-19 15:09:14 +1100

    update css and home

---
+++ client/copsNotes/www/js/modules/homeMenu.js
@@ -0,0 +1,17 @@
+copsNotes.module("Home.Menu",function(Menu,copsNotes,Backbone,Marionette,$,_){
+
+    /*view delcaration*/
+
+    Menu.HomeMenuView = Marionette.ItemView.extend({
+        template:"#homeMenu_tmpl",
+        tagName:"div"
+    });
+
+    /*controller*/
+    Menu.controller = {
+        showHomeMenu:function(){
+            var homeMenuView = new copsNotes.Home.Menu.HomeMenuView();
+            copsNotes.menu.show(homeMenuView);
+        }
+    };
+});
